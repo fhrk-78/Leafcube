9e8ae30982bb62976423885af373b12464722922
haru2277 <haru20senmori@outlook.com>
2023-03-04 08:49:48 +0900

    とりあえず

--- main.cpp
+++ main.cpp
@@ -12,26 +12,39 @@ std::string processName[1024];
 int processAccesspermission[1024];
 std::string processPublisher[1024];
 
+//初期化
+bool processManagerEnable = false;
+
 //関数
 int processAdd(std::string name, int permission, std::string publisher) { //プロセスの追加
-    int i = 0;
-    while (processName[i] != "0") {
-        i += 1;
-        if (i > 1024) {
-            return -1;
+    if (processManagerEnable == true) {
+        int i = 0;
+        while (processName[i] != "0") {
+            i += 1;
+            if (i > 1024) {
+                return -1;
+            }
         }
+        processName[i] = name;
+        processAccesspermission[i] = permission;
+        processPublisher[i] = publisher;
+        return 0;
     }
-    processName[i] = name;
-    processAccesspermission[i] = permission;
-    processPublisher[i] = publisher;
-    return 0;
+    else {
+        return -1;
+    }
 }
 
-int processsPecifiedAdd(int numloc, std::string name, int permission, std::string publisher) { //プロセスの追加
-    processName[numloc] = name;
-    processAccesspermission[numloc] = permission;
-    processPublisher[numloc] = publisher;
-    return 0;
+int processsPecifiedAdd(int numloc, std::string name, int permission, std::string publisher) { //プロセスの追加 (番号指定)
+    if (processManagerEnable == true) {
+        processName[numloc] = name;
+        processAccesspermission[numloc] = permission;
+        processPublisher[numloc] = publisher;
+        return 0;
+    }
+    else {
+        return -1;
+    }
 }
 
 //処理メソッド
@@ -46,6 +59,8 @@ void initialize() { //初期化
     for (auto i = 0; i < 1024; ++i) {
         processPublisher[i] = "0";
     }
+
+    processManagerEnable = true;
 
     //各処理をプロセスとして登録
     processsPecifiedAdd(1001, "system/processManager", 10, "system/system_process");
